364f2131f82838babd96b4706f3b72ce0f31ee0f
ritik0002 <ritik_d@hotmail.com>
2022-03-29 16:14:38 +0100

    css file

--- src/components/viewCalendar/viewCalendar.js
+++ src/components/viewCalendar/viewCalendar.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-
+import "./Calendar.css"
 export default class viewCalendar extends Component {
-  render() {
-    return (
-      <FullCalendar
-      plugins={[ dayGridPlugin ]}
-      initialView="dayGridMonth"
-      events={[
+  constructor() {
+    super();
+    this.state = {
+      Events: [
+        // initial event data
         { title: 'Booking 3', date: '2022-04-01' },
         { title: 'Booking 4', date: '2022-04-01' },
         { title: 'Booking 5', date: '2022-04-02' },
         { title: 'Booking 1', date: '2022-03-05' },
         { title: 'Booking 2', date: '2022-03-29' }
-      ]}
+      ]
+    };
+  }
+  render() {
+    return (
+      <FullCalendar
+      plugins={[ dayGridPlugin ]}
+      initialView="dayGridMonth"
+      events={this.state.Events}
     />
     )
   }
