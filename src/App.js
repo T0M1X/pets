81db472a5bb59d93e5d9fbb7cd6441083b835c72
d-arde <dardeleanu00@gmail.com>
2022-04-02 15:47:05 +0100

    started adding buttons, working on css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import {Container} from "./components/styles/Container.styled"
-import { ThemeProvider } from 'styled-components'
-import GlobalStyles from './components/styles/Global'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainPageWrapper from './components/styles/MainPage.styled'
-import pawPrint from './images/pawPrint.png'
-import housePrint from './images/housePrint.png'
-import rainbowPrint from './images/rainbowPrint.png'
-import petCat from './images/petCat.png'
-import petDog from './images/dogHappy.jpeg' // put images in pub
-import petCat2 from './images/petCat2.jpeg'
-
+import { Container } from "./components/styles/Container.styled";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./components/styles/Global";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainPageWrapper from "./components/styles/MainPage.styled";
+import pawPrint from "./images/pawPrint.png";
+import housePrint from "./images/housePrint.png";
+import rainbowPrint from "./images/rainbowPrint.png";
+import petCat from "./images/petCat.png";
+import petDog from "./images/dogHappy.jpeg"; // put images in pub
+import petCat2 from "./images/petCat2.jpeg";
 
 const theme = {
   colors: {
-    header: 'rgb(24,26,27)',
-    body: 'rgb(200,200,200)',
-    footer: 'rgb(24,26,27)',
+    header: "rgb(24,26,27)",
+    body: "rgb(200,200,200)",
+    footer: "rgb(24,26,27)",
   },
-  mobile: '768px',
-}
+  mobile: "768px",
+};
 
 function App() {
   return (
@@ -31,54 +30,72 @@ function App() {
           <Header />
           <Routes>
             <Route
-            path="/"
-            element={
-              <>
-                <Container>
-                  <div>
-                    {/* get buttons to link to page */}
-                  <p className='blankText'>l</p>
-                  <p className='blankText'>l</p>
-                  <p className='blankText'>l</p>
-                  <p className='blankText'>l</p>
-                  <p className='blankText'>l</p>
-                  <p className='openingText'>Welcome to PetFriends, where all pets are friends here!</p>
-                  <p className='underlyingText'>Book the best sitters and walkers for your furry friends</p>
-                  <p className='blankText'>l</p>
-                  <p className='blankText'>l</p>
-                  <p className='blankText'>l</p>
-                  <p className='blankText'>l</p>
-                  <p className='blankText'>l</p>
-                  </div>
-                </Container>
-                <MainPageWrapper>
-                
-                <h2 className='infoTitle'>Services for every pet</h2>
-                <div className='grid'>
-                  <img className='petCat' src={petCat} ></img>
-                  <img className='petDog' src={petDog} ></img>
-                  <p>Here are some of our satisfied customers!</p>
-                </div>
-                 <div className='infoDivs'>
-                  <img className = 'pawImg' src={pawPrint}></img>
-                  <h4>Walking</h4>
-                  <p>We will walk your pet near your area or where ever you feel like! Perfect for your pet to stretch its legs, or wings; whatever they would like.</p>
-                </div>
-                <div className='infoDivs'>
-                    <img className = 'houseImg' src={housePrint}></img>
-                    <h4>Drop-in</h4>
-                    <p>Your pet visits your sitter's home to take care, play, feed and have fun with them! </p>
-                </div>
-                <div className='infoDivs'>
-                  <img className = 'rainbowImg' src={rainbowPrint}></img>
-                  <h4>Day Care</h4>
-                  <p>Your sitter takes care of your pets and home, giving your furry friends all the attention and love they need without leaving the home.</p>
-                </div>
-                
-                </MainPageWrapper>
-                
-              </>
-            }/>
+              path="/"
+              element={
+                <>
+                  <Container>
+                    <div>
+                      {/* get buttons to link to page */}
+                      <p className="blankText">l</p>
+                      <p className="blankText">l</p>
+                      <p className="blankText">l</p>
+                      <p className="blankText">l</p>
+                      <p className="blankText">l</p>
+                      <p className="openingText">
+                        Welcome to PetFriends, where all pets are friends here!
+                      </p>
+                      <p className="underlyingText">
+                        Book the best sitters and walkers for your furry friends
+                      </p>
+                      {/*buttons will go here*/}
+                      <div className="button-group">
+                        <button>Walking</button>
+                        <button>Day Care</button>
+                      </div>
+                      <p className="blankText">l</p>
+                      <p className="blankText">l</p>
+                      <p className="blankText">l</p>
+                      <p className="blankText">l</p>
+                      <p className="blankText">l</p>
+                    </div>
+                  </Container>
+                  <MainPageWrapper>
+                    <h2 className="infoTitle">Services for every pet</h2>
+                    <div className="grid">
+                      <img className="petCat" src={petCat}></img>
+                      <img className="petDog" src={petDog}></img>
+                      <p>Here are some of our satisfied customers!</p>
+                    </div>
+                    <div className="infoDivs">
+                      <img className="pawImg" src={pawPrint}></img>
+                      <h4>Walking</h4>
+                      <p>
+                        We will walk your pet near your area or where ever you
+                        feel like! Perfect for your pet to stretch its legs, or
+                        wings; whatever they would like.
+                      </p>
+                    </div>
+                    <div className="infoDivs">
+                      <img className="houseImg" src={housePrint}></img>
+                      <h4>Drop-in</h4>
+                      <p>
+                        Your pet visits your sitter's home to take care, play,
+                        feed and have fun with them!{" "}
+                      </p>
+                    </div>
+                    <div className="infoDivs">
+                      <img className="rainbowImg" src={rainbowPrint}></img>
+                      <h4>Day Care</h4>
+                      <p>
+                        Your sitter takes care of your pets and home, giving
+                        your furry friends all the attention and love they need
+                        without leaving the home.
+                      </p>
+                    </div>
+                  </MainPageWrapper>
+                </>
+              }
+            />
           </Routes>
           <Footer />
         </>
--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -1,32 +1,58 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
+  background-image: url(cats.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover !important;
 
-    background-image: url(cats.jpg);
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover!important;
+  p {
+    padding: 10px;
+  }
 
-    p{
-        padding: 10px;
-    }
+  .blankText {
+    opacity: 0;
+  }
 
-    .blankText {
-        opacity: 0;
-    }
+  .openingText {
+    text-align: center;
+    color: white;
+    font-size: 40px;
+  }
 
-    .openingText{
-        text-align: center;
-        color: white;
-        font-size:40px;
-        
-    }
+  .underlyingText {
+    text-align: center;
+    color: white;
+    font-size: 25px;
+    padding: 0px;
+  }
 
-    .underlyingText{
-        text-align: center;
-        color: white;
-        font-size:25px;
-        padding: 0px;
+  .button-group button {
+    display: inline-block;
+    background-color: grey;
+    color: gainsboro;
+    float: left;
+    margin-top: 20px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+    font-size: 15px;
+  }
 
-    }
-`
+  .button-group {
+    margin: auto;
+    padding: 10px;
+    width: 50%;
+  }
+
+  .button-group button:not(:last-child) {
+    border-right: none;
+  }
+
+  .button-group:after {
+    content: "";
+    clear: both;
+    display: table;
+  }
+`;
