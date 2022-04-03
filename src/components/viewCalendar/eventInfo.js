aad4f352800d23a89c30ad8e2e4bb6b482cac289
ritik0002 <ritik_d@hotmail.com>
2022-04-03 04:59:26 +0100

    Able to select a calender and View booking details

--- src/components/viewCalendar/eventInfo.js
+++ src/components/viewCalendar/eventInfo.js
@@ -16,26 +16,36 @@ export default class eventInfo extends Component {
       <div className="Event-container">
           <div></div>
       <div className="title">Booking Infomation</div>
-      <hr></hr>
+
       <div className="booking-container">
-      <div>Booking ID: {this.props.event.id}</div>
-      <div>Booking Title:{this.props.event.title}</div>
-      <div>Start Date:{(this.props.event.start).toString().substring(25,0)}</div>
+      <div><h3>Booking ID: </h3> {this.props.event.id}</div>
+      <div><h3>Start Date: </h3>{(this.props.event.start).toString().substring(25,0)}</div>
+      <div><h3>End Date: </h3> {(this.props.event.end).toString().substring(25,0)}</div>
+      <div><h3>Booking Title: </h3>{this.props.event.title}</div>
+      </div>
       <span className="close" onClick={this.handleClick}>x</span>
-      <div>End Date:{(this.props.event.end).toString().substring(25,0)}</div>
-      <div className="Info">
-      <div>Name:{this.props.event.extendedProps.name}</div>
-      <div>{this.props.event.extendedProps.address}</div>
-      <div>{this.props.event.extendedProps.Number}</div>
+      <div className="Flex-Container">
+      <div className="info-container">
+      <h2 className="Client-Info">Client Infomation</h2>
+      <div><h3>Name: </h3> {this.props.event.extendedProps.name}</div>
+      <div className="Address"><h3>Address: </h3>{this.props.event.extendedProps.Address}</div>
+      <div><h3>Phone Number: </h3> {this.props.event.extendedProps.Number}</div>
       </div>
-      <div className="Pet-Container">
-      <div>{this.props.event.extendedProps.typeBooking}</div>
-      <div>{this.props.event.extendedProps.typePet.join(', ')}</div>
-      <div>{this.props.event.extendedProps.amount}</div>
+      <div className="pet-container">
+      <h2 className="Pet-Info">Appointment Details</h2>
+      <div><h3>Booking Type:</h3> {this.props.event.extendedProps.typeBooking} Booking</div>
+      <div><h3>Pet Type: </h3>{this.props.event.extendedProps.typePet.join(', ')}</div>
+      <div><h3>Amount of Pets: </h3> {this.props.event.extendedProps.amount}</div>
       </div>
+      </div>
+      <div className="Flex-Container-2">
       <div className="Other">
-        <h1>Additional Information</h1>
+        <h2>Additional Information</h2>
         {this.props.event.extendedProps.other}
+      </div>
+      <div className="Payment-container">
+          <h2>Payment Details</h2>
+          <div><h3>Total Cost: </h3>£{this.props.event.extendedProps.Payment}</div>
       </div>
       </div>
       </div>
--- src/components/viewCalendar/viewCalendar.js
+++ src/components/viewCalendar/viewCalendar.js
@@ -12,11 +12,11 @@ export default class viewCalendar extends Component {
       display:false,
       Events: [
         // initial event data
-        {id:1, title: 'Booking 3', start: '2022-04-01T14:30:00' ,end:'2022-04-01T17:30:00',name:"Eren Yeager",Address:"60 Main Road London SE25 8LQ",Number:"07700 900471",typeBooking:"Sitting",typePet:["French bull dog","goldfish"],amount:2,other:"Give medication every 2 hours "},
-        {id:2, title: 'Booking 4', start: '2022-04-01 12:30:00' ,end:'2022-04-01T13:30:00',name:"Eren Yeager",Address:"60 Main Road London SE25 8LQ",Number:"07700 900471",typeBooking:"Sitting",typePet:["French bull dog","goldfish"],amount:2,other:"Give medication every 2 hours "},
-        {id:3, title: 'Booking 5', start: '2022-04-02 08:30:00',end:'2022-04-02T10:30:00',name:"Eren Yeager",Address:"60 Main Road London SE25 8LQ",Number:"07700 900471",typeBooking:"Sitting",typePet:["French bull dog","goldfish"],amount:2,other:"Give medication every 2 hours "},
-        {id:4,title: 'Booking 1', start: '2022-03-05 10:30:00',end:'2022-03-05T13:30:00',name:"Eren Yeager",Address:"60 Main Road London SE25 8LQ",Number:"07700 900471",typeBooking:"Sitting",typePet:["French bull dog","goldfish"],amount:2,other:"Give medication every 2 hours "},
-        {id:5, title: 'Booking 2', start: '2022-03-29 21:30:00' ,end:'2022-03-29T23:30:00',name:"Eren Yeager",Address:"60 Main Road London SE25 8LQ",Number:"07700 900471",typeBooking:"Sitting",typePet:["French bull dog","goldfish"],amount:2,other:"Give medication every 2 hours "},
+        {id:"1A1SF3", title: 'Booking 3', start: '2022-04-01T14:30:00' ,end:'2022-04-01T17:30:00',name:"Eren Yeager",Address:"60 Main Road, London, SE25 8LQ",Number:"07700 900471",typeBooking:"Sitting",typePet:["French bull dog","Goldfish"],amount:2,other:"Give medication every 2 hours ",Payment:30.53},
+        {id:"2A2DGGY", title: 'Booking 4', start: '2022-04-01 12:30:00' ,end:'2022-04-01T13:30:00',name:"Eren Yeager",Address:"60 Main Road, London, SE25 8LQ",Number:"07720 81471",typeBooking:"Sitting",typePet:["Golden Retriever","rabbit"],amount:2,other:"Give medication every 2 hours ",Payment:20.21},
+        {id:"1DSGF3", title: 'Booking 5', start: '2022-04-02 08:30:00',end:'2022-04-02T10:30:00',name:"Eren Yeager",Address:"60 Main Road, London, SE25 8LQ",Number:"07703 821421",typeBooking:"Walking",typePet:["Elephant","Giraffe","French iguana","rattlesnake","Transformer"],amount:4,other:"Give medication every 2 hours ",Payment:125.12},
+        {id:"DGHRT4",title: 'Booking 1', start: '2022-03-05 10:30:00',end:'2022-03-05T13:30:00',name:"Eren Yeager",Address:"60 Main Road, London, SE25 8LQ",Number:"07788 910431",typeBooking:"Sitting",typePet:["StarFish"],amount:1,other:"Give medication every 2 hours ",Payment:11.50},
+        {id:"124FG5", title: 'Booking 2', start: '2022-03-29 21:30:00' ,end:'2022-03-29T23:30:00',name:"Eren Yeager",Address:"60 Main Road, London, SE25 8LQ",Number:"07721 124541",typeBooking:"Walking",typePet:["Lynx Cat"],amount:1,other:"Give medication every 2 hours ",Payment:88.12},
       ]
     };
 
