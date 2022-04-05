00b874b8b53cd6a2712551cc4adce58e852511bb
ritik0002 <ritik_d@hotmail.com>
2022-04-05 19:19:25 +0100

    review form done

--- src/components/writeReview/writeReview.js
+++ src/components/writeReview/writeReview.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'
 import "./Review.css"
+import { Link } from "react-router-dom";
 
 export default class writeReview extends Component {
   constructor(props){
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);    
+    
     this.state={
-      description:'',
+      text:'',
     }
   }
 
   handleChange(event) {
-    this.setState({description: event.target.value});
+    this.setState({text: event.target.value});
   }
 
     handleSubmit (event){
       event.preventDefault();
-      alert("The name you entered was:"+this.state.description);
+      alert("The name you entered was:"+this.state.text);
+
     }
 
   render() {
@@ -25,37 +28,70 @@ export default class writeReview extends Component {
       <div className="Background">
         <div className="Container-title">
         <div className="review-title">Submit your Review</div>
-      <form className="ReviewhtmlForm" onSubmit={this.handleSubmit}>
+      <form className="ReviewForm" onSubmit={this.handleSubmit}>
+      <h2>Rate Your expierence</h2>
         <div className="Rating-Container">
-        <label>Rate Your expierence</label>
-        <div>Quality:
-        <div className="rate">
+        <div class="quality">Quality of service</div>
+        <div>
         <fieldset className="rating">
-    <input type="radio" id="star5" name="rating" value="5" /><label class = "full" htmlFor="star5" title="Awesome - 5 stars"></label>
-    <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" htmlFor="star4half" title="Pretty good - 4.5 stars"></label>
-    <input type="radio" id="star4" name="rating" value="4" /><label class = "full" htmlFor="star4" title="Pretty good - 4 stars"></label>
-    <input type="radio" id="star3half" name="rating" value="3 and a half" /><label class="half" htmlFor="star3half" title="Meh - 3.5 stars"></label>
-    <input type="radio" id="star3" name="rating" value="3" /><label class = "full" htmlFor="star3" title="Meh - 3 stars"></label>
-    <input type="radio" id="star2half" name="rating" value="2 and a half" /><label class="half" htmlFor="star2half" title="Kinda bad - 2.5 stars"></label>
-    <input type="radio" id="star2" name="rating" value="2" /><label class = "full" htmlFor="star2" title="Kinda bad - 2 stars"></label>
-    <input type="radio" id="star1half" name="rating" value="1 and a half" /><label class="half" htmlFor="star1half" title="Meh - 1.5 stars"></label>
-    <input type="radio" id="star1" name="rating" value="1" /><label class = "full" htmlFor="star1" title="Sucks big time - 1 star"></label>
-    <input type="radio" id="starhalf" name="rating" value="half" /><label class="half" htmlFor="starhalf" title="Sucks big time - 0.5 stars"></label>
-</fieldset>
-      </div>
-
+    <input type="radio" id="q5" name="Quality" value="5" /><label  htmlFor="q5" title="5 stars"></label>
+    <input type="radio" id="q4" name="Quality" value="4" /><label htmlFor="q4" title="4 stars"></label>
+    <input type="radio" id="q3" name="Quality" value="3" /><label  htmlFor="q3" title="3 stars"></label>
+    <input type="radio" id="q2" name="Quality" value="2" /><label  htmlFor="q2" title="2 stars"></label>
+    <input type="radio" id="q1" name="Quality" value="1" /><label  htmlFor="q1" title="1 star"></label>
+        </fieldset>
+        </div>
+        <div class="quality">Price</div>
+        <div>
+        <fieldset className="rating">
+          <input type="radio" id="p5" name="Price" value="5" /><label  htmlFor="p5" title="5 stars"></label>
+          <input type="radio" id="p4" name="Price" value="4" /><label  htmlFor="p4" title="4 stars"></label>
+          <input type="radio" id="p3" name="Price" value="3" /><label  htmlFor="p3" title="3 stars"></label>
+          <input type="radio" id="p2" name="Price" value="2" /><label  htmlFor="p2" title="2 stars"></label>
+          <input type="radio" id="p1" name="Price" value="1" /><label  htmlFor="p1" title="1 star"></label>
+        </fieldset>
+        </div>
+        <div class="quality">Welfare</div>
+        <div>
+        <fieldset className="rating">
+          <input type="radio" id="w5" name="Welfare" value="5" /><label  htmlFor="w5" title="5 stars"></label>
+          <input type="radio" id="w4" name="Welfare" value="4" /><label  htmlFor="w4" title="4 stars"></label>
+          <input type="radio" id="w3" name="Welfare" value="3" /><label  htmlFor="w3" title="3 stars"></label>
+          <input type="radio" id="w2" name="Welfare" value="2" /><label  htmlFor="w2" title="2 stars"></label>
+          <input type="radio" id="w1" name="Welfare" value="1" /><label  htmlFor="w1" title="1 star"></label>
+        </fieldset>
+        </div>
+        <div class="quality">Timeliness</div>
+        <div>
+        <fieldset className="rating">
+          <input type="radio" id="t5" name="Timeliness" value="5" /><label  htmlFor="t5" title="5 stars"></label>
+          <input type="radio" id="t4" name="Timeliness" value="4" /><label  htmlFor="t4" title="4 stars"></label>
+          <input type="radio" id="t3" name="Timeliness" value="3" /><label  htmlFor="t3" title="3 stars"></label>
+          <input type="radio" id="t2" name="Timeliness" value="2" /><label  htmlFor="t2" title="2 stars"></label>
+          <input type="radio" id="t1" name="Timeliness" value="1" /><label  htmlFor="t1" title="1 star"></label>
+        </fieldset>
         </div>
         </div>
 
 
         <div className="recommend-container"> 
-        
+        <h2>Would you recommend this product?</h2>
+        <div class="button-container">
+        <input type="radio" id="Yes" name="recommend" value="yes" checked/>
+        <label htmlFor="Yes">Yes</label>
+        <input type="radio" id="No" name="recommend" value="no"/>
+        <label htmlFor="No">No</label>
+        </div>
         </div>
         <div className="Description">
-          <div><label>Description</label></div>
-        <textarea id="desc" name="description"placeholder='Enter review'></textarea>
+          <h2>Description</h2>
+        <textarea id="desc" name="description"placeholder='Enter review' value={this.state.text} onChange={this.handleChange} required></textarea>
       </div>
+      <div>
+        <Link to="/viewCalendar">
       <button type="submit" className="submitButton">Submit</button>
+      </Link>
+      </div>
     </form>
     </div>
     </div>
