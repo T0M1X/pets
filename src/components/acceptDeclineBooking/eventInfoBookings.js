0e006299813b558f26fb1691dbd970bd82478195
d-arde <dardeleanu00@gmail.com>
2022-04-13 20:49:27 +0100

    added console logs to see if buttons work

--- src/components/acceptDeclineBooking/eventInfoBookings.js
+++ src/components/acceptDeclineBooking/eventInfoBookings.js
@@ -11,16 +11,21 @@ class eventInfoBookings extends Component {
       bookings: booked,
       user: UserMethods.GetUserById(localStorage.getItem("UserId")).username, // gets username of the current sitter logged in
       sitterID: UserMethods.GetUserById(localStorage.getItem("UserId")).id, //uses methods from other files to get the sitter ID
-    
     };
   }
-  
+
   bookingStatus = (id, isAccepted) => (event) => {
     //this function checks to see if the data in the json is "Booked" or "Not Booked"
     // ... and so changes the state
     if (isAccepted) {
+      {
+        console.log("accept button");
+      }
       BookingMethods.acceptBookingById(id);
     } else {
+      {
+        console.log("decline button");
+      }
       BookingMethods.declineBookingById(id);
     }
     this.setState({ bookings: booked });
@@ -32,89 +37,75 @@ class eventInfoBookings extends Component {
           <script></script>
           <br></br>
           <div>
-            {console.log("hello")}
             {console.log(this.state.sitterID)}
             <h1>List of Bookings for {this.state.user}</h1>
           </div>
-          {console.log(BookingMethods.GetBookingBySitterId(this.state.sitterID)[0]?.id)}
-         
-          {BookingMethods.GetBookingBySitterId(this.state.sitterID)[0]?.id!=null?
-          <div>
-          
-           {BookingMethods.GetBookingBySitterId(this.state.sitterID).map((booking,i) => {
-              
-            {console.log("hello2")}
-             {console.log(booking)}
-             {console.log(booking.id)}
-             {console.log(i)}
-             return (
-             <div key={i}>
-          <div className="bookingDiv">
-            <div className="cardDiv">
-              <table>
-                <tbody>
-                  <tr>
-                    <td>
-                      <p style={{ fontSize: "15px" }}>
-                        ID of booking:
-                        {
-                          booking.id
-                        }
-                      </p>
-                      <p>
-                        {
-                          booking.name
-                        }
-                      </p>
-                      <p>
-                        {
-                          booking.address
-                        }
-                      </p>
-                      <p
-                        style={{
-                          color:
-                            booking?.accepted === "Booked"
-                              ? "green"
-                              : "red", // here is where the function matters, it checks if the text in bookings.accepted fits the conditions
-                        }}
-                      >
-                        Is Booked? -{" "}
-                        {
-                          booking.accepted
-                        }
-                      </p>
-                      <br></br>
-                      <button
-                        onClick={this.bookingStatus(
-                         booking.id,
-                          true
-                        )}
-                      >
-                        Accept
-                      </button>
-                      <button
-                        onClick={this.bookingStatus(
-                          BookingMethods.GetBookingBySitterId(
-                            this.state.sitterID
-                          )?.id,
-                          false
-                        )}
-                      >
-                        Decline
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+          {console.log(
+            BookingMethods.GetBookingBySitterId(this.state.sitterID)[0]?.id
+          )}
+
+          {BookingMethods.GetBookingBySitterId(this.state.sitterID)[0]?.id !=
+          null ? (
+            <div>
+              {BookingMethods.GetBookingBySitterId(this.state.sitterID).map(
+                (booking, i) => {
+                  return (
+                    <div key={i}>
+                      <div className="bookingDiv">
+                        <div className="cardDiv">
+                          <table>
+                            <tbody>
+                              <tr>
+                                <td>
+                                  <p style={{ fontSize: "15px" }}>
+                                    ID of booking:
+                                    {booking.id}
+                                  </p>
+                                  <p>{booking.name}</p>
+                                  <p>{booking.address}</p>
+                                  <p
+                                    style={{
+                                      color:
+                                        booking?.accepted === "Booked"
+                                          ? "green"
+                                          : "red", // here is where the function matters, it checks if the text in bookings.accepted fits the conditions
+                                    }}
+                                  >
+                                    Is Booked? - {booking.accepted}
+                                  </p>
+                                  <br></br>
+                                  <button
+                                    onClick={this.bookingStatus(
+                                      booking.id,
+                                      true
+                                    )}
+                                  >
+                                    Accept
+                                  </button>
+                                  <button
+                                    onClick={this.bookingStatus(
+                                      BookingMethods.GetBookingBySitterId(
+                                        this.state.sitterID
+                                      )?.id,
+                                      false
+                                    )}
+                                  >
+                                    Decline
+                                  </button>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                }
+              )}
             </div>
-          </div>
-            </div>
-            );
-          })}
-            
-            </div>                    
-          :<div className="book-error">No bookings avaliable</div>}
+          ) : (
+            <div className="book-error">No bookings avaliable</div>
+          )}
         </div>
       </StyledBooking>
     );
